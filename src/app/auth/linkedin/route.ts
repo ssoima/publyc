d050379e6abe1ac85dd00483721bf2172b193006
ssoima <sergiu.soima@gmail.com>
2024-11-24 16:13:43 -0800

    updated linkedin login for posting

---
+++ src/app/auth/linkedin/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from 'next/server';
+import {HOSTNAME} from "@/utils/constants";
+
+export async function GET(request: NextRequest) {
+    const clientId = process.env.LINKEDIN_CLIENT_ID as string;
+    const redirectUri = encodeURIComponent(`${HOSTNAME}/auth/linkedin/callback`);
+    const state = 'random_string'; // Replace with a secure random string
+    const scope = encodeURIComponent('r_liteprofile r_emailaddress w_member_social');
+
+    const authorizationUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}`;
+
+    return NextResponse.redirect(authorizationUrl);
+}
