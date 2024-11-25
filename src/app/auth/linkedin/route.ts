529e847f8b9b11f4ca0709e5e0ef7b88eaccaaa3
ssoima <sergiu.soima@gmail.com>
2024-11-24 17:06:25 -0800

    solved hostname issue with env variable

--- src/app/auth/linkedin/route.ts
+++ src/app/auth/linkedin/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: NextRequest) {
     const clientId = process.env.LINKEDIN_CLIENT_ID as string;
     const redirectUri = encodeURIComponent(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/linkedin/callback`);
     const state = 'random_string'; // Replace with a secure random string
-    const scope = encodeURIComponent('r_liteprofile r_emailaddress w_member_social');
+    const scope = encodeURIComponent('liteprofile emailaddress w_member_social');
 
     const authorizationUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}`;
 
